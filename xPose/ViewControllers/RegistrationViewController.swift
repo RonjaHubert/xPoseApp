49142d194a587824f42222aecd69813c51ca1843
RonjaHubert <103991087+RonjaHubert@users.noreply.github.com>
2022-04-24 19:57:18 +0200

    Login & Registrierung

---
+++ xPose/ViewControllers/RegistrationViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RegistrationViewController.swift
+//  xPose
+//
+//  Created by Ronja Hubert on 24.04.22.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        contentView.layer.cornerRadius = 20
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
